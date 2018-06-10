63ceb575edfc95f6372628359c4a73d43e9bf6ad
James Kim <jhlink@users.noreply.github.com>
2018-06-09 21:42:11 -0400

    fix: Correct type -> typeof within getMetricMetaInfo function

--- utils/helpers.js
+++ utils/helpers.js
@@ -140,7 +140,7 @@ export function getMetricMetaInfo(metric) {
     }, 
   };
 
-  return type metric === 'undefined' 
+  return typeof metric === 'undefined' 
     ? info 
-    : info[metric]
+    : info[metric];
 }
