a509460a790237cb7726d8c9e3e5d4bf689ae73c
James Kim <jhlink@users.noreply.github.com>
2018-06-09 22:03:56 -0400

    feat: Show icons from getMetricMetaInfo within AddEntry component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,7 +6,7 @@ export default class AddEntry extends Component {
   render() {
     return (
       <View> 
-        {getMetricMetaInfo('bike')}
+        {getMetricMetaInfo('bike').getIcon()}
       </View>
     );
   }
