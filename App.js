df7e4039fb16d48fdec345c13068037dfae8576d
James Kim <jhlink@users.noreply.github.com>
2018-06-09 22:03:05 -0400

    feat: Add AddEntry component in App component" git status

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {  View } from 'react-native';
+import React, { Component }  from 'react';
+import { View } from 'react-native';
 import AddEntry from './components/AddEntry';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View>
