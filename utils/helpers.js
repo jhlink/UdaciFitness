a68b4e0b10fd7d488942cbf29324d89563972f47
James Kim <jhlink@users.noreply.github.com>
2018-06-09 21:31:56 -0400

    feat: Implement getMetricMetaInfo method

--- utils/helpers.js
+++ utils/helpers.js
@@ -46,3 +46,97 @@ export function timeToString (time = Date.now()) {
   return todayUTC.toISOString().split('T')[0];
 }
 
+export function getMetricMetaInfo() {
+  const info = {
+    run: { 
+      displayName: 'Run',
+      max: 50,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialIcons
+              name='directions-run'
+              color={'black'}
+              size={35}
+            />
+          </View>
+        );
+      }
+    },
+    bike: { 
+      displayName: 'Bike',
+      max: 100,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons
+              name='directions-bike'
+              color={'black'}
+              size={35}
+            />
+          </View>
+        );
+      }
+    },
+    swim: { 
+      displayName: 'Swim',
+      max: 9900,
+      unit: 'meters',
+      step: 100,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons
+              name='swim'
+              color={'black'}
+              size={35}
+            />
+          </View>
+        );
+      }
+    },
+    sleep: { 
+      displayName: 'Sleep',
+      max: 24,
+      unit: 'hours',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <FontAwesome
+              name='bed'
+              color={'black'}
+              size={35}
+            />
+          </View>
+        );
+      }
+    }, 
+    eat: { 
+      displayName: 'Eat',
+      max: 10,
+      unit: 'rating',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons
+              name='food'
+              color={'black'}
+              size={35}
+            />
+          </View>
+        );
+      }
+    }, 
+  };
+}
