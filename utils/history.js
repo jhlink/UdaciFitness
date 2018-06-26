cf02f608075bb181ae9346ceadf34dfd6085228e
James Kim <jhlink@users.noreply.github.com>
2018-06-26 08:26:34 -0400

    feat: Create history component w/ boilerplate

---
+++ utils/history.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class History extends Component {
+  render() {
+    return (
+      <View>
+        <Text>History</Text>
+      </View>
+    );
+  }
+}
+
+export default History;
+
+
