f29721085eb73853421a8d50a8ad3b3b445b7fa8
James Kim <jhlink@users.noreply.github.com>
2018-06-26 10:12:44 -0400

    feat: Style and return DateHeader within History component

--- components/History.js
+++ components/History.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+import UdaciFitnessCalendar from 'udacifitness-calendar';
 import { connect } from 'react-redux';
+
+import DateHeader from './DateHeader'; 
 import { receiveEntries, addEntry } from '../actions';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
 import { fetchCalendarResults } from '../utils/api';
-import UdaciFitnessCalendar from 'udacifitness-calendar';
+import { white } from '../utils/colors';
 
 class History extends Component {
   componentDidMount() {
@@ -22,9 +25,15 @@ class History extends Component {
   }
 
   renderItem = ({ today, ...metrics}, formattedDate, key) => ( 
-    <View>
+    <View style={styles.item}>
       {today
-        ? <Text>{JSON.stringify(today)}</Text>
+        ? 
+        <View>
+          <DateHeader date={formattedDate} />
+          <Text >
+            {JSON.stringify(today)}
+          </Text>
+        </View>
         : <Text>{JSON.stringify(metrics)}</Text>
       }
     </View>
@@ -50,6 +59,25 @@ class History extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  }
+});
 
 function mapStateToProps(entries) {
   return {
