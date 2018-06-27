18a4dae6db0770b197cebd052b0cca7bb0a1fe82
James Kim <jhlink@users.noreply.github.com>
2018-06-27 11:50:28 -0400

    refactor: Remove deadcode from App.js"

--- App.js
+++ App.js
@@ -1,63 +1,13 @@
 import React, { Component }  from 'react';
 import { 
-  Text,
-  View,
-  StyleSheet, 
-  TouchableHighlight, 
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  Platform
+  View
 } from 'react-native';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
-import AddEntry from './components/AddEntry';
 import History from './components/History';
 import styled from 'styled-components/native';
-
-function touchableDemo () {
-  return (
-    <View style={styles.container}>
-      <TouchableHighlight 
-        style={styles.btn} 
-        onPress={this.handlePress} 
-        underlayColor='#d4271b'>
-        <Text style={styles.btnText}>
-            Touchable Highlight
-        </Text>
-      </TouchableHighlight>
-
-      <TouchableOpacity 
-        style={styles.btn} 
-        onPress={this.handlePress}>
-        <Text style={styles.btnText}>
-            Touchable Opacity 
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableWithoutFeedback 
-        onPress={this.handlePress}>
-        <View style={styles.btn}>
-          <Text style={styles.btnText}>
-            Touchable Feedback 
-          </Text>
-        </View>
-      </TouchableWithoutFeedback>
-
-      <TouchableNativeFeedback 
-        background={TouchableNativeFeedback.SelectableBackground()}>
-        <View style={styles.btn}>
-          <Text style={styles.btnText}>
-            Touchable Native Feedback 
-          </Text>
-        </View>
-      </TouchableNativeFeedback>
-         
-    </View>
-  );
-}
 
 const CenterView = styled.View`
   flex: 1;
@@ -86,4 +36,5 @@ export default class App extends Component {
     );
   }
 }
+
 
