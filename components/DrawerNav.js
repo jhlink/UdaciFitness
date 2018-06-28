1ce86121b68a929c1e030f272634d143c9d26f00
James Kim <jhlink@users.noreply.github.com>
2018-06-28 03:36:59 -0400

    feat: Create DrawerNav component

---
+++ components/DrawerNav.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+import { createDrawerNavigator } from 'react-navigation';
+import { white, purple } from '../utils/colors';
+
+const Home = ({ navigation }) => ( 
+  <View style={styles.container}>
+    <Text style={styles.btnText} >This is the Home view</Text>
+    <TouchableOpacity 
+      style={styles.btn}
+      onPress={() => navigation.openDrawer()}>
+      <Text style={styles.btnText}>Press here for the Dashboard</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+function Dashboard ({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is the Dashboard</Text>
+    </View> 
+  );
+}
+
+const DrawerNav = createDrawerNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: 'Home'
+    }
+  },
+  EntryDetail: {
+    screen: Dashboard,
+    navigationOptions: {
+      headerTintColor: white, 
+      headerStyle: {
+        backgroundColor: purple, 
+      }
+    }
+  }
+
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#999999'
+  },
+  btn: {
+    backgroundColor: '#E53224',
+    marginTop: 10,
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#fff'
+  }
+});
+
+export default DrawerNav;
