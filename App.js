5b5ffdca90d401839053ef5f78d2c777e2c66807
James Kim <jhlink@users.noreply.github.com>
2018-06-28 02:51:56 -0400

    refactor: Remove encapsulated navigation code

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component }  from 'react';
 import { 
   View,
-  Platform,
   StatusBar
 } from 'react-native';
 
@@ -9,57 +8,11 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 
-import History from './components/History';
-import AddEntry from './components/AddEntry';
+import StackNav from './components/StackNav';
 
 import styled from 'styled-components/native';
-import { createBottomTabNavigator } from 'react-navigation';
-import { purple, white } from './utils/colors';
-import { FontAwesome, Ionicons }  from '@expo/vector-icons';
+import { purple } from './utils/colors';
 import { Constants } from 'expo';
-
-const Tabs = createBottomTabNavigator({
-  History: {
-    screen: History,
-    navigationOptions: {
-      tabBarLabel: 'History',
-      tabBarIcon: ({ tintColor }) => <Ionicons 
-        name='ios-bookmarks' 
-        size={30} 
-        color={tintColor} />
-    }
-  },
-  AddEntry: {
-    screen: AddEntry,
-    navigationOptions: {
-      tabBarLabel: 'Add Entry',
-      tabBarIcon: ({ tintColor }) => <FontAwesome 
-        name='plus-square' 
-        size={30} 
-        color={tintColor} />
-    }
-  }
-}, {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? purple : white,
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ? white : purple,
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-}
-);
-
 
 function UdaciStatusBar ({ backgroundColor, ...props }) {
   return (
@@ -90,7 +43,7 @@ export default class App extends Component {
       <Provider store={createStore(reducer)}>
         <CenterView>
           <UdaciStatusBar backgroundColor={ purple } barStyle='light-content'/>
-          <Tabs />
+          <StackNav />
         </CenterView>
       </Provider>
     );
