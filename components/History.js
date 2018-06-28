47480145c42e6e35ff54a3f8767207c069264437
James Kim <jhlink@users.noreply.github.com>
2018-06-28 02:55:16 -0400

    feat: Pass navigation prop within History component for new entry

--- components/History.js
+++ components/History.js
@@ -43,7 +43,10 @@ class History extends Component {
             {JSON.stringify(today)}
           </Text>
         </View>
-        : <TouchableOpacity onPress={() => console.log('Pressed!')}>
+        : <TouchableOpacity onPress={() => this.props.navigation.navigate(
+          'EntryDetail',
+          { entryId: key }
+        )}>
           <MetricCard metrics={metrics} date={formattedDate}/>  
         </TouchableOpacity>
       }
