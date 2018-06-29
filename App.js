5c367192d51b8dde0ca320823dd9176c5c662e47
James Kim <jhlink@users.noreply.github.com>
2018-06-29 03:02:13 -0400

    feat: Implement pickImage handler with supporting Image handling components

--- App.js
+++ App.js
@@ -3,12 +3,39 @@ import {
   Text,
   View,
   StyleSheet,
+  ImageEditor,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
+import { ImagePicker } from 'expo';
 
 import styled from 'styled-components/native';
 import { purple } from './utils/colors';
 
 export default class App extends Component {
+  state = {
+    image: null,
+  }
+
+  pickImage = () => {
+    ImagePicker.launchImageLibraryAsync({
+      allowEditting: true,
+      aspect: [2, 1]
+    }).then((result) => {
+      if (result.cancelled) {
+        return;
+      }
+
+      ImageEditor.cropImage(result.uri, {
+        offset: {x: 0, y: 0},
+        size: { width: result.width, height: result.height },
+        displaySize: { width: 200, height: 100},
+        resizeMode: 'contain',
+      }, 
+      (uri) => this.setState(() => ({ image: uri })),
+      () => console.log('Euror'));
+    });  
+  }
 
   render() {
     return (
