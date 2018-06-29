68919b77d40d07a318bff0bdc5f3677239fe5414
James Kim <jhlink@users.noreply.github.com>
2018-06-29 02:06:18 -0400

    feat: Add BounceAnimation to direction heading in Live component

--- components/Live.js
+++ components/Live.js
@@ -4,7 +4,8 @@ import {
   Text,
   ActivityIndicator,
   TouchableOpacity,
-  StyleSheet
+  StyleSheet,
+  Animated
 } from 'react-native';
 import { Foundation } from '@expo/vector-icons';
 import { purple, white } from '../utils/colors';
@@ -16,6 +17,7 @@ export default class Live extends Component {
     coords: null,
     status: null,
     direction: '',
+    bounceValue: new Animated.Value(1)
   }
 
   componentDidMount() {
@@ -52,7 +54,14 @@ export default class Live extends Component {
       distanceInterval: 1,
     }, ({ coords }) => {
       const newDirection = calculateDirection(coords.heading); 
-      const { direction } = this.state;
+      const { direction, bounceValue } = this.state;
+
+      if (newDirection !== direction) {
+        Animated.sequence([
+          Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+          Animated.spring(bounceValue, { toValue: 1, friction: 4})
+        ]).start();
+      }
 
       this.setState(() => ({
         coords,
@@ -63,7 +72,7 @@ export default class Live extends Component {
   }
 
   render() {
-    const { status, coords, direction } = this.state;
+    const { status, coords, direction, bounceValue } = this.state;
 
     if (status === null) {
       return <ActivityIndicator style={{marginTop: 30}} />;
@@ -107,7 +116,10 @@ export default class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>{direction}</Text>
+          <Animated.Text 
+            style={[styles.direction, {transform: [{scale: bounceValue}]}]}>
+            {direction}
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
