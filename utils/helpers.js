da5ba384ae7d1a3c6eb0ec14ea5d79c2822beb71
James Kim <jhlink@users.noreply.github.com>
2018-06-29 02:23:32 -0400

    feat: Implement setLocalNotification within helpers file

--- utils/helpers.js
+++ utils/helpers.js
@@ -186,7 +186,32 @@ function createNotification() {
   };
 }
 
-
 export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then( JSON.parse )
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync();
 
+              let tomorrow = new Date();
+              tomorrow.setDate(tomorrow.getDate() + 1);
+              tomorrow.setHours(20);
+              tomorrow.setMinutes(0);
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              );
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+            }
+          });
+      }
+    });
 }
